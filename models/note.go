42d23a50878c4bc8274057afbb6c7b543b98831b
Xeney <jeneksero@gmail.com>
2024-10-24 02:10:41 +0400

    Чистая структура проекта

--- models/note.go
+++ models/note.go
@@ -1,10 +1,1 @@
 package models
-
-type Note struct {
-	// Уникальный идентификатор заметки, автоматически инкрементируемый
-	ID int `json:"id" gorm:"primaryKey;autoIncrement"`
-	// Заголовок заметки
-	Title string `json:"title"`
-	// Содержание заметки
-	Content string `json:"content"`
-}
